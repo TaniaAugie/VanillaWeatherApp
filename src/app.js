0e1edad70111e2cbec6ad18a53a7e49e63edd8c4
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2023-07-17 22:56:15 +0200

    updated

---
+++ src/app.js
@@ -0,0 +1,12 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+}
+
+let apiKey = "d3b3247o775abt0c69f7b0e4513c11ea";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Johannesburg&key=d3b3247o775abt0c69f7b0e4513c11ea&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
