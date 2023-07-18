dee6a3dc40a0c120b3e1ba893070642f96f239d0
TaniaAugie <123157567+TaniaAugie@users.noreply.github.com>
2023-07-18 21:57:28 +0200

    Updated weather

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temp.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "d3b3247o775abt0c69f7b0e4513c11ea";
